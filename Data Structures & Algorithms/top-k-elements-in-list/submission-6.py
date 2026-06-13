7da5b42bedfe1326177d2777f695724b8d83fda2
alam-a <21148736+alam-a@users.noreply.github.com>
2026-06-14 00:29:54 +0530

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = defaultdict(int)
+        for num in nums:
+            freq[num] += 1
+        sparse_lists = [[] for _ in range(len(nums) + 1)]
+        for key, val in freq.items():
+            sparse_lists[val].append(key)
+        res = []
+        for sparse_list in sparse_lists[::-1]:
+            res.extend(sparse_list)
+            if len(res) >= k:
+                break
+        return res
